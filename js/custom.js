333a5661541d2338d7349f4c28151f8890a94fe2
bortojac <bortojac@gmail.com>
2017-10-22 12:05:26 -0400

    delete toggle function - no need for toggle button with new layout

--- js/custom.js
+++ js/custom.js
@@ -1,10 +1,2 @@
-function toggleMenu() {
-    console.log(document.getElementById("sidebar").style.display);
-    var sidebar = document.getElementById("sidebar");
-    if (sidebar.style.display === "none" || sidebar.style.display === "") {
-        sidebar.style.display = "flex";
-    } else {
-        sidebar.style.display = "none";
-    }
-}
+
 

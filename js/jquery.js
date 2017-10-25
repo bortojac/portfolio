5a28057ac43e392cce009f78030f0943a69ec7c1
bortojac <bortojac@gmail.com>
2017-10-24 23:51:45 -0400

    styling, background-blend-mode, cleaning up some css

--- js/jquery.js
+++ js/jquery.js
@@ -1,14 +1,15 @@
-$(document).ready(function(){
+$(document).ready(
+	function(){
 	$('a[href^="#"]').on('click',function (e) {
 	    e.preventDefault();
-
+		/*this.hash reads the href attribute of this, and gets the part of the URL beginning with # */
 	    var target = this.hash;
-	    var $target = $(target);
 
 	    $('html, body').stop().animate({
-	        'scrollTop': $target.offset().top
+	        'scrollTop': $(target).offset().top
 	    }, 900, 'swing', function () {
 	        window.location.hash = target;
 	    });
 	});
-})
+});
+

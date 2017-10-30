8884af41f36c2f584bdf682d3f4e69a3a96e91bc
bortojac <bortojac@gmail.com>
2017-10-29 22:28:58 -0400

    new image, toggle button on left side - may change

--- js/jquery.js
+++ js/jquery.js
@@ -1,15 +1,22 @@
 $(document).ready(
-	function(){
-	$('a[href^="#"]').on('click',function (e) {
-	    e.preventDefault();
-		/*this.hash reads the href attribute of this, and gets the part of the URL beginning with # */
-	    var target = this.hash;
+	function() {
+		$('a[href^="#"]').hide();
+		// show the circle links when they click Projects
+		$('#navProjects').on('click', function (e) {
+			$('a[href^="#"]').toggle();			
+			return false;
+		});
+		// animate the scroll	
+		$('a[href^="#"]').on('click', function (e) {
+			e.preventDefault();
+			/*this.hash reads the href attribute of this, and gets the part of the URL beginning with # */
+			var target = this.hash;
 
-	    $('html, body').stop().animate({
-	        'scrollTop': $(target).offset().top
-	    }, 900, 'swing', function () {
-	        window.location.hash = target;
-	    });
+			$('html, body').stop().animate({
+				'scrollTop': $(target).offset().top
+			}, 500, 'swing', function () {
+				window.location.hash = target;
+			});
+		});
 	});
-});
 

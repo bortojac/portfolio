faf87e262f4e5963dd769cd0106b720a0723d938
bortojac <bortojac@gmail.com>
2017-10-31 00:22:45 -0400

    js to fade project content from bottom on scroll

--- js/jquery.js
+++ js/jquery.js
@@ -1,9 +1,34 @@
 $(document).ready(
 	function() {
+		// create fade from bottom
+		var $animation_elements = $('.animateThese');
+		var $window = $(window);
+		console.log($animation_elements);
+		function check_if_in_view() {
+		  var window_height = $window.height();
+		  var window_top_position = $window.scrollTop();
+		  var window_bottom_position = (window_top_position + window_height);
 		
+		  $.each($animation_elements, function() {
+			var $element = $(this);
+			var element_height = $element.outerHeight();
+			var element_top_position = $element.offset().top;
+			var element_bottom_position = (element_top_position + element_height);
+		
+			//check to see if this current container is within viewport
+			if ((element_bottom_position >= window_top_position) &&
+			  (element_top_position <= window_bottom_position)) {
+			  $element.addClass('in-view');
+			} else {
+			  $element.removeClass('in-view');
+			}
+		  });
+		}
+		
+		$window.on('scroll resize', check_if_in_view);
+		$window.trigger('scroll');
 		// animate the scroll	
 		$('a[href^="#"]').on('click', function (e) {
-			console.log("this worked");
 			e.preventDefault();
 			/*this.hash reads the href attribute of this, and gets the part of the URL beginning with # */
 			var target = this.hash;

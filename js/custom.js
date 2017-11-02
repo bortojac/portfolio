39a956642c4c4a234ed0ec7d02b383f167f007c6
bortojac <bortojac@gmail.com>
2017-11-02 00:00:01 -0400

    add vhFix from stanko.github.io to help with url resizing on chrome iOS

--- js/custom.js
+++ js/custom.js
@@ -1,2 +1,18 @@
 
-
+var options = [
+    {
+      selector: '.projects > section', // Mandatory, CSS selector
+      vh: 100,  // Mandatory, height in vh units
+    },
+   {
+      selector: 'main > article',
+      vh: 100
+    }//,
+    /*{
+      selector: '.projects > section',
+      vh: 100
+    }*/
+  ];
+  
+  var vhFix = new VHChromeFix(options);
+  

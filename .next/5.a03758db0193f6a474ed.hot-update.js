6780b9aa31c4d63cd747031d2f20add24dc7551e
bortojac <bortojac@gmail.com>
2018-03-21 00:45:00 -0400

    first commit
    - playing around with pages

---
+++ .next/5.a03758db0193f6a474ed.hot-update.js
@@ -0,0 +1,12 @@
+webpackHotUpdate(5,{
+
+/***/ "./node_modules/next/link.js":
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__("./node_modules/next/dist/lib/link.js")
+
+
+/***/ })
+
+})
+//# sourceMappingURL=5.a03758db0193f6a474ed.hot-update.js.map
---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Link from 'next/link';
+import Layout from '../components/Layout';
+
+const App = () => {
+    return (
+        <Layout>
+        <p>Hello Next.js</p>
+     </Layout>
+    )
+}
+
+export default App
